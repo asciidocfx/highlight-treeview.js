76b9e1478400cc50a4d9be71306c07dde828e916
ozlerhakan <ozler.hakan@gmail.com>
2015-03-21 10:47:49 +0200

    Add missing file extensions and improve path resolving

--- treeview/highlight-treeview.js
+++ treeview/highlight-treeview.js
@@ -37,58 +37,58 @@ hljs.registerLanguage('treeview', function(hljs) {
         relevance : 0
       },
       {
-        className : 'file photo',
-        begin : /[^ \n\t  ].+\.(bmp|eps|gif|jpg|jpe(g)?|png|svg|tif(f)?|ico|tga|targa|j2(k|c)|jng)/,
-        relevance : 0
-      },
-      {
         className : 'file plain',
-        begin : /[^ \n\t  ].+\.(cfg|conf(ig)?|sh|bat|csv|ini|log|md|info|txt|asc(iidoc)?|ad(oc)?|tex|pom|properties|lst|project|iml)/,
+        begin : /[^ \n\t  ].+\.(epub(3)?|mobi|azw(3)?|cfg|conf(ig)?|sh|bat|csv|ini|log|md|markdown|info|txt|asc(iidoc)?|ad(oc)?|tex|pom|properties|lst|project|iml)$/,
         relevance : 0
       },
       {
         className : 'file video',
-        begin : /[^ \n\t  ].+\.(cda|avi|flv|mkv|mov|mp4|mpeg|mpg|ogv|webm|divx|wmv)/,
+        begin : /[^ \n\t  ].+\.(cda|avi|flv|mkv|mov|mp4|mpeg|mpg|ogv|webm|divx|wmv)$/,
         relevance : 0
       },
       {
         className : 'file source',
-        begin : /[^ \n\t  ].+\.(asp(x)?|c(pp|c|s(s)?|lass)?|h(tm(l)?|(h)?)|jav(a)?|js|aj|php|rb|xml|yml)/,
+        begin : /[^ \n\t  ].+\.(asp(x)?|c(pp|c|s(s)?|lass)?|h(tm(l)?|(h)?)|jav(a)?|js(on)?|aj|php|rb|xml|yml)$/,
+        relevance : 0
+      },
+      {
+        className : 'file photo',
+        begin : /[^ \n\t  ].+\.(bmp|eps|gif|jpg|jpe(g)?|png|svg|tif(f)?|ico|tga|targa|j2(k|c)|jng)$/,
         relevance : 0
       },
       {
         className : 'file audio',
-        begin : /[^ \n\t  ].+\.(aac|au|cda|flac|mp(3|2)|oga|ogg|wav|wma|flac|ac3|mac)/,
+        begin : /[^ \n\t  ].+\.(aac|au|cda|flac|mp(3|2)|oga|ogg|wav|wma|flac|ac3|mac)$/,
         relevance : 0
       },
       {
         className : 'file archive',
-        begin : /[^ \n\t  ].+\.(7z|bz(2)?|gz|rar|tar|tgz|zip|repo|jar)/,
+        begin : /[^ \n\t  ].+\.(7z|bz(2)?|gz|rar|tar|tgz|zip|repo|jar)$/,
         relevance : 0
       },
       {
         className : 'file pdf',
-        begin : /[^ \n\t  ].+\.(pdf)/,
+        begin : /[^ \n\t  ].+\.(pdf)$/,
         relevance : 0
       },
       {
         className : 'file xls',
-        begin : /[^ \n\t  ].+\.(xls(x)?)/,
+        begin : /[^ \n\t  ].+\.(xls(x)?)$/,
         relevance : 0
       },
       {
         className : 'file doc',
-        begin : /[^ \n\t  ].+\.(doc(m|x)?)/,
+        begin : /[^ \n\t  ].+\.(doc(m|x)?)$/,
         relevance : 0
       },
       {
         className : 'file ppt',
-        begin : /[^ \n\t  ].+\.(pps|ppt(x)?)/,
+        begin : /[^ \n\t  ].+\.(pps|ppt(x)?)$/,
         relevance : 0
       },
       {
         className : 'file plain',
-        begin : /readme|README|LICENSE|license/,
+        begin : /(readme|README|LICENSE|license|CNAME)$/,
         relevance : 0
       },
       {
